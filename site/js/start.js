e0b8fb5e28fa7e36249ed4e4f744faee635c6d44
Jason Iqbal <jasoniqbal@gmail.com>
2023-01-13 20:57:10 -0800

    fix resize and add default css over user agent fallbacks for consistency

--- site/js/start.js
+++ site/js/start.js
@@ -7,16 +7,19 @@ function init() {
   
   let gameCanvas = document.getElementById('gameCanvas');
   let controlPanel = document.getElementById('controlPanel');
-  let scaleFactor = { x : 1, y : 1 };
+  let gameTitle = document.getElementById('captionTitle');
+  let gameBody = document.getElementsByTagName("body").item(0);
+  let gameRoot = document.getElementById('gameRoot');
   let mapSize = { width : 3300, height : 2550 };
+  let scaleFactor = { x : 1, y : 1 };
   
-  gameCanvas.width = window.innerWidth - controlPanel.clientWidth;
-  gameCanvas.height = window.innerHeight;
+  gameCanvas.width = gameRoot.clientWidth - controlPanel.clientWidth - 16 - 4;
+  // - body margin x2 - table data padding x2 - second table data padding x2;  
+  gameCanvas.height = gameRoot.clientHeight - 16 - gameTitle.clientHeight - 4 - 2;
+  // - body margin x2 - caption title height - caption margin x2 - table data padding x2 
+  
   let ctx = gameCanvas.getContext('2d');
-  
-  scaleFactor.x = gameCanvas.width / mapSize.width;
-  scaleFactor.y = scaleFactor.x;
-  
+  scaleFactor = getScaleFactor();
   ctx.scale(scaleFactor.x, scaleFactor.y);  
   
   window.addEventListener('resize', onWindowResize);
@@ -28,21 +31,32 @@ function init() {
   function onWindowResize() {
 //    console.log("resize");
     // make sure scope is proper
-
-    ctx.scale( 1 / scaleFactor.x, 1 / scaleFactor.y );
     
-    gameCanvas.width = window.innerWidth - controlPanel.clientWidth;
-    gameCanvas.height = window.innerHeight;
+    gameCanvas.width = gameRoot.clientWidth - controlPanel.clientWidth - 16 - 4; 
+    // - body margin x2 - table data padding x2 - second table data padding x2;
+    gameCanvas.height = gameRoot.clientHeight - 16 - gameTitle.clientHeight - 4 - 2;
+    // - body margin x2 - caption title height - caption margin x2 - table data padding x2 
     
-    scaleFactor.x = gameCanvas.width / mapSize.width;
-    scaleFactor.y = scaleFactor.x;
-  
+    scaleFactor = getScaleFactor();
     ctx.scale(scaleFactor.x, scaleFactor.y); 
     
     if (game.my.ready) {
       game.render();
     }
-    
   }
+  
+  function getScaleFactor() {
+    let factor;
+    let wFactor = gameCanvas.width / mapSize.width;
+    let hFactor = gameCanvas.height / mapSize.height;
+    
+    factor = (wFactor <= hFactor)? wFactor : hFactor;
+
+    return {
+      x : factor,
+      y : factor
+    }
+  }
+  
 }
 init();
